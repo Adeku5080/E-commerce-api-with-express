7bb842c6d7dba66f6a97e26013cc2ceb1e9deefd
Ali Adeku <aliadeku.aam@gmail.com>
2023-02-08 03:55:24 +0100

    Create Product Schema

---
+++ src/model/Product.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide a product name"],
+    maxLength: [100, "Name cannot be more than 100 characters"],
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: [true, "Please product price"],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+    maxLength: [1000, "Name cannot be more than 1000 characters"],
+  },
+  image: {
+    type: String,
+    default: "/upload/examples.jpeg",
+  },
+  category: {
+    type: String,
+    required: [true, "Please provide a product category"],
+    enum: ["office", "Kitchen", "bedroom"],
+  },
+  company: {
+    type: String,
+    required: [true, "Please provide a product company"],
+    enum: {
+      values: ["ikea", "liddy", ",marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
+  colors: {
+    type: [String],
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  freeShipping: {
+    type: Boolean,
+    default: false,
+  },
+  inventory: {
+    type: Number,
+    required: true,
+    default: 15,
+  },
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+},{timestamps:true});
+
+module.exports = mongoose.model("Product", ProductSchema);
